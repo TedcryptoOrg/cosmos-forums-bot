cbade75c720a4e4838c6bb615e1f7c57bf7f179e
Jose Lopes <josluis.lopes@gmail.com>
2023-03-13 13:22:01 +0000

    Added astroport

---
+++ src/forum/provider/AstroPortForum.ts
@@ -0,0 +1,57 @@
+import {ForumProvider} from "./ForumProvider";
+import {Article} from "../../types/Article";
+import {MD5} from 'crypto-js';
+import axios from "axios";
+import {ForumProviders} from "../../enums/ForumProviders";
+
+const Parser = require('rss-parser');
+
+export class AstroPortForum implements ForumProvider {
+    private parser: typeof Parser;
+    private currentHash: string;
+
+    constructor() {
+        this.parser = new Parser();
+        this.currentHash = '';
+    }
+
+    async getArticles(): Promise<Article[]> {
+        const feedText = (await axios.get('https://forum.astroport.fi/latest.rss')).data;
+        const feed = await this.parser.parseString(feedText);
+        const feedHash = MD5(feedText).toString();
+        if (feedHash === this.currentHash) {
+            console.log('[AstroPort Forum] No new articles found.')
+            return [];
+        }
+
+        this.currentHash = feedHash;
+        let articles: Article[] = [];
+        if (!feed.hasOwnProperty('items')) {
+            console.log('[AstroPort Forum] No articles found.');
+            return [];
+        }
+
+        for (const item of feed.items) {
+            if (!item.categories.find((str: string) => str.includes('ARC'))) {
+                console.debug('[AstroPort Forum] Skipping article: ' + item.title + ' with categories: ' + item.categories.join(', '));
+                continue;
+            }
+            articles.push({
+                title: item.title,
+                url: item.link,
+                community: 'astroport',
+                provider: this.getName(),
+            });
+        }
+
+        return articles;
+    }
+
+    getCommunities(): string[] {
+        return ['astroport'];
+    }
+
+    getName(): ForumProviders {
+        return ForumProviders.AstroPortForum;
+    }
+}
